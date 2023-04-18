a07af8a915d9a276c9f1c0bef7ef4c75a2cc6996
Jay Hammond <45923533+jay4170@users.noreply.github.com>
2023-04-18 22:04:59 +0100

    First working iteration

--- PasswordGenerator/Program.cs
+++ PasswordGenerator/Program.cs
@@ -1,37 +1,116 @@
 ﻿using System;
-namespace PasswordGenerator {
 
+namespace PasswordGenerator
+{
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome how long would you like your password to be?");
-            int length;
-            length = Console.ReadLine();
-            if (length ==null)
+            int length = 0;
+
+            Console.WriteLine("Welcome, how long would you like your password to be?");
+
+            while (!int.TryParse(Console.ReadLine(), out length))
             {
-                return;
+                Console.WriteLine("Try again with a number");
             }
+
             if (length < 8)
             {
-                
+                Console.WriteLine("Length needs to be at least 8.");
+                length = 8;
             }
-            Console.WriteLine("Creating a password {length} digits long");
-            string password;
-            password = passwordGen(length);
+
+
+            Console.WriteLine("Creating a password " + length + " characters long");
+            string password = PasswordGen(length);
+            Console.WriteLine("Your password is: " + password);
 
         }
-        static string passwordGen(int length)
+
+        static string PasswordGen(int length)
         {
-            string generatedPassword = "";
-            string upperLetter = makeUpperLetter();
-            string lowerLetter = makeUpperLetter();
-            string randNumber = makeNumber();
-            string special = makeSpecial();
+            char[] chars = new char[length];
+            chars[0] = makeUpperLetter();
+            chars[1] = makeLowerLetter();
+            chars[2] = makeNumber();
+            chars[3] = makeSpecial();
 
+            Random rand = new Random();
 
-            return generatedPassword;
+            for (int i = 4; i < length; i++)
+            {
+                int num = rand.Next(0, 4);
+                char letter;
+                switch (num)
+                {
+                    case 0:
+                        letter = makeLowerLetter();
+                        break;
+                    case 1:
+                        letter = makeUpperLetter();
+                        break;
+                    case 2:
+                        letter = makeNumber();
+                        break;
+                    default:
+                        letter = makeSpecial();
+                        break;
+                }
+                chars[i] = letter;
+            }
+
+            var rng = new Random();
+            rng.Shuffle(chars);
+
+            return new string(chars);
+        }
+
+        static char makeUpperLetter()
+        {
+            string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            Random rand = new Random();
+            int num = rand.Next(0, chars.Length);
+            return chars[num];
+        }
+
+        static char makeLowerLetter()
+        {
+            string chars = "abcdefghijklmnopqrstuvwxyz";
+            Random rand = new Random();
+            int num = rand.Next(0, chars.Length);
+            return chars[num];
+        }
+
+        static char makeNumber()
+        {
+            string chars = "1234567890";
+            Random rand = new Random();
+            int num = rand.Next(0, chars.Length);
+            return chars[num];
+        }
+
+        static char makeSpecial()
+        {
+            string chars = "$%#@!*?;:^&";
+            Random rand = new Random();
+            int num = rand.Next(0, chars.Length);
+            return chars[num];
         }
     }
 
+    public static class RandomExtensions
+    {
+        public static void Shuffle<T>(this Random rng, T[] array)
+        {
+            int n = array.Length;
+            while (n > 1)
+            {
+                int k = rng.Next(n--);
+                T temp = array[n];
+                array[n] = array[k];
+                array[k] = temp;
+            }
+        }
+    }
 }
